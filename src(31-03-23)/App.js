d380ce227881acb1b15e84343cc8aaffa8129196
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2023-03-31 21:50:05 -0300

    Add files via upload

---
+++ src(31-03-23)/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+//import Toolbar from './Toolbar';
+//import ButtonCustom from './ButtonCustom';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Gallery />
+        <Gallery />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src(31-03-23)/ButtonCustom.js
@@ -0,0 +1,12 @@
+
+export default function ButtonCustom() {
+    function handleClick() {
+        alert('You clicked me!');
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Click me
+        </button>
+    );
+}
