eb8acb755ff1020346405cfaa220dd30a33032f9
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2023-04-14 21:40:06 -0300

    Add files via upload

---
+++ aula6/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cadastro from './Cadastro';
+import ExemploChat from './ExemploChat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ExemploChat/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
