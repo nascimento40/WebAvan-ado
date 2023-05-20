a70b9d82bd4673c4c3e3cbd1872054e96857927c
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2023-05-19 21:31:40 -0300

    Add files via upload

---
+++ aula8/backend-node.js/servidor_express_http.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const movies = require('./moviesdb.json');
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.get('/', function(req, res) {
+  res.send('API Catálogo de filmes disponível!');
+});
+
+app.post('/api', function(req, res) {
+  res.json({texto_enviado: req.body.texto});
+});
+
+app.get('/api/movies/id', function(req, res) {
+    var id_movie = req.query.id
+    res.json({listmoveis: movies});
+});
+
+app.get('/api/movies', function(req, res) {
+  res.json({listmoveis: movies});
+});
+
+app.listen(3001, function() {
+  console.log('App rodando na porta 3001!'); 
+}); 
